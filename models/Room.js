7d020187cc25be96450c65209335ab26418ec393
Владимир Свербинский <vladimirsverbinskiy@MacBook-Pro-Vladimir.local>
2020-10-18 17:12:19 +0300

    create model Room

--- models/Room.js
+++ models/Room.js
@@ -1,11 +1,13 @@
 const { Schema, model } = require('mongoose')
 
 const roomSchema = new Schema({
-    name: { type: String, default: Date.now.toString },
-    type: { type: String, default: 'STD', required: true },
+    name: { type: String, required: true },
+    children: { type: Boolean, required: true, default: false },
+    sharing: { type: Boolean, required: true, default: false },
+    type: { type: String, required: true },
     accommodation: { type: String, required: true },
-    description: { type: String, required: false },    
-    price: { type: Number, default: 0, required: true }
+    price: { type: Number, required: true },
+    hotelId: { type: Schema.Types.ObjectId, ref: 'Hotel' }
 })
 
 module.exports = model('Room', roomSchema)
